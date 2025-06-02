b91f4490ca8b7c7bfdda069472da7354b53731b1
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-06-02 17:47:06 +0300

    Add project files.

---
+++ KairaCQRSMediator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KairaCQRSMediator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
