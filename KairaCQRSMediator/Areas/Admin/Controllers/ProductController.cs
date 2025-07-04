e218edfb12de989f9d6904817518651636e5cd7b
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-07-04 16:41:04 +0300

    Add product management feature in admin area
    This commit introduces a new feature for managing products in the KairaCQRSMediator application. Key changes include:
    
    - Updated _ViewImports.cshtml to include product results.
    - Modified GetCategoryQueryHandler to handle product types.
    - Created ProductController with an Index action to list products.
    - Developed Index.cshtml to display product details and actions.
    
    Users can now view, update, delete, and create products within the admin area.

---
+++ KairaCQRSMediator/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using KairaCQRSMediator.Features.Mediator.Queries.ProductQueries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KairaCQRSMediator.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController(IMediator mediator) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var product = await mediator.Send(new GetProductsQuery());
+            return View(product);
+        }
+    }
+}
--- KairaCQRSMediator/Features/CQRS/Handlers/CategoryHandlers/GetCategoryQueryHandler.cs
+++ KairaCQRSMediator/Features/CQRS/Handlers/CategoryHandlers/GetCategoryQueryHandler.cs
@@ -1,5 +1,6 @@
 ﻿using KairaCQRSMediator.DataAccess.Entities;
 using KairaCQRSMediator.Features.CQRS.Results.CategoryResults;
+using KairaCQRSMediator.Features.Mediator.Results.ProductResults;
 using KairaCQRSMediator.Repositories;
 
 namespace KairaCQRSMediator.Features.CQRS.Handlers.CategoryHandlers
@@ -22,8 +23,7 @@ namespace KairaCQRSMediator.Features.CQRS.Handlers.CategoryHandlers
                 Id = c.Id,
                 Name = c.Name,
                 ImageUrl = c.ImageUrl,
-                Products = c.Products
-            }).ToList();
+                Products = c.Products as IList<GetProductsQueryResult>            }).ToList();
 
             return dtos;
         }
