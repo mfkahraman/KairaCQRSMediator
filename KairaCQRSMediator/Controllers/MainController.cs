d2f6a2febf79e65d57c1e7eb16527b7ea4e7f42f
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-07-11 16:33:03 +0300

    homepage created

---
+++ KairaCQRSMediator/Controllers/MainController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KairaCQRSMediator.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult Homepage()
+        {
+            return View();
+        }
+
+    }
+}
