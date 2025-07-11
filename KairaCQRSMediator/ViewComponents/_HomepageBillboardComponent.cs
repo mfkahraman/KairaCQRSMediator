a81765425b4d9713cbeb3f70db5306120f1f8f02
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-07-11 16:45:38 +0300

    HomepageHeadComponent and HomepageTopBarComponent and their views are created

---
+++ KairaCQRSMediator/ViewComponents/_HomepageBillboardComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KairaCQRSMediator.ViewComponents
+{
+    public class _HomepageBillboardComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ KairaCQRSMediator/ViewComponents/_HomepageTopBarComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KairaCQRSMediator.ViewComponents
+{
+    public class _HomepageTopBarComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
