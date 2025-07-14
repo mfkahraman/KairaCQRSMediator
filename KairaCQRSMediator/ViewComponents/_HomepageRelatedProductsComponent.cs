ad301d8917aafc3c5107904646d92c8461a74a6f
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-07-14 15:45:19 +0300

    _HomepageRelatedProductsComponent done

---
+++ KairaCQRSMediator/ViewComponents/_HomepageRelatedProductsComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KairaCQRSMediator.ViewComponents
+{
+    public class _HomepageRelatedProductsComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
