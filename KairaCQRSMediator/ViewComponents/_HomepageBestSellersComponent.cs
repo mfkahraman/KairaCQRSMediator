a823873958d77cd0bd315e0d32a68119d8decdb8
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-07-14 15:39:21 +0300

    _HomepageBestSellersComponent done

---
+++ KairaCQRSMediator/ViewComponents/_HomepageBestSellersComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KairaCQRSMediator.ViewComponents
+{
+    public class _HomepageBestSellersComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
