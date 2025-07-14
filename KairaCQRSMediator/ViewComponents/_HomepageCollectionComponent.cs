9f546d434026f0a8d9322dd4b80b97c2c3aad487
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-07-14 15:37:33 +0300

    _HomepageCollectionComponent created

---
+++ KairaCQRSMediator/ViewComponents/_HomepageCollectionComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KairaCQRSMediator.ViewComponents
+{
+    public class _HomepageCollectionComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
