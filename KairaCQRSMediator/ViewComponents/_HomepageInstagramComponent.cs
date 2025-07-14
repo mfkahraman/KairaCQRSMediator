e8028632e570f1c79703b5b00a71bd110df2845a
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-07-14 15:52:33 +0300

    _HomepageInstagramComponent done

---
+++ KairaCQRSMediator/ViewComponents/_HomepageInstagramComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KairaCQRSMediator.ViewComponents
+{
+    public class _HomepageInstagramComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
