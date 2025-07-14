85e084fa8dd204c828950bfa0ec10a2ccbdb5f9c
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-07-14 15:54:22 +0300

    _HomepageFooterComponent done

---
+++ KairaCQRSMediator/ViewComponents/_HomepageFooterComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KairaCQRSMediator.ViewComponents
+{
+    public class _HomepageFooterComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
