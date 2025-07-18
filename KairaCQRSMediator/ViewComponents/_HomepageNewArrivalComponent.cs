70ef18392bacc402517718b5f760b495cd2ad463
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-07-18 16:25:28 +0300

    In the New Arrivals section, the last 4 products are displayed dynamically by fetching them from the database

--- KairaCQRSMediator/ViewComponents/_HomepageNewArrivalComponent.cs
+++ KairaCQRSMediator/ViewComponents/_HomepageNewArrivalComponent.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KairaCQRSMediator.Features.Mediator.Queries.ProductQueries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace KairaCQRSMediator.ViewComponents
 {
-    public class _HomepageNewArrivalComponent : ViewComponent
+    public class _HomepageNewArrivalComponent(IMediator mediator) : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+            var lastFourProducts = (await mediator.Send(new GetProductsQuery()))
+                .OrderByDescending(p => p.Id)
+                .Take(4)
+                .ToList();
+            return View(lastFourProducts);
         }
     }
 }
