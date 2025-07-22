5e1e74f4cce47199688045791d880a5a7cbc5bfc
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-07-22 14:08:52 +0300

    Admin subscribecontroller created. listing subscribes on index and delete subscribe features added

---
+++ KairaCQRSMediator/Areas/Admin/Controllers/SubscribeController.cs
@@ -0,0 +1,23 @@
+﻿using KairaCQRSMediator.Features.CQRS.Handlers.SubscribeHandlers;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace KairaCQRSMediator.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SubscribeController(GetSubscribesQueryHandler getHandler,
+                                     RemoveSubscribeCommandHandler removeHandler) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var subscribes = await getHandler.Handle();
+            return View(subscribes);
+        }
+
+        public async Task<IActionResult> DeleteSubscribe(int id)
+        {
+            var result = await removeHandler.HandleAsync(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
